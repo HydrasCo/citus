7814bdd8624874b1fda6c640b1944351901b420f
Jonathan Dance <wuputah@users.noreply.github.com>
2022-10-07 16:43:21 -0500

    re-create the C functions as the libname changed

---
+++ src/backend/columnar/sql/columnar--11.1-2--11.1-3.sql
@@ -0,0 +1,7 @@
+-- re-create the C functions to tie them to the new libname
+
+#include "udfs/columnar_handler/10.0-1.sql"
+#include "udfs/alter_columnar_table_set/10.0-1.sql"
+#include "udfs/alter_columnar_table_reset/10.0-1.sql"
+#include "udfs/upgrade_columnar_storage/10.2-1.sql"
+#include "udfs/downgrade_columnar_storage/10.2-1.sql"
